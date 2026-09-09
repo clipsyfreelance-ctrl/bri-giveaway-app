d6cfa9870509ed61e2ffb7425d4cc0882864b9c5
Nama Kamu <email@kamu.com>
2026-09-10 00:38:32 +0700

    first commit: BRI Giveaway App

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bri.giveaway',
+  appName: 'BRImo Giveaway',
+  webDir: 'dist'
+};
+
+export default config;
